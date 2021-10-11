369a97f42ae3de6b00ee43a7e00c6ee1b7ea24ca
Anurag Mishra <55075448+AnuragMishra2311@users.noreply.github.com>
2021-10-11 22:18:16 +0530

    add text cleaning file

---
+++ models/text_cleaning.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+from collections import defaultdict
+tokenizer = RegexpTokenizer(r'\w+')
+stop_words = stopwords.words('english')
+
+class text_processing:
+    
+    def __init__(self, my_dataframe):
+        '''
+         my_dataframe : pandas dataframe, all changes made in this dataframe will 
+                        reflect back in original one (changes are in_place). 
+        '''
+
+        self.my_dataframe = my_dataframe
+        
+    # Removes puncutations and tokenize the sentence.
+    def tokenize_helper(self, words):
+        words = str(words).lower()
+        words = tokenizer.tokenize(words)
+        return words
+    
+    def tokenize(self):
+        self.my_dataframe['question1'] = self.my_dataframe['question1'].apply(self.tokenize_helper)
+        self.my_dataframe['question2'] = self.my_dataframe['question2'].apply(self.tokenize_helper)
+            
+            
+    # Remove Stopwords 
+    def stopword_helper(self, words):
+        words = [w for w in words if w not in stop_words]
+        return words
+            
+    def remove_stop_words(self):
+        self.my_dataframe['question1'] = self.my_dataframe['question1'].apply(self.stopword_helper)
+        self.my_dataframe['question2'] = self.my_dataframe['question2'].apply(self.stopword_helper)
+        
+        
+    # Lemmitiazation with POS tagging
+    def lemmi_helper(self, tokens):
+        
+        tag_map = defaultdict(lambda : wn.NOUN)
+        tag_map['J'] = wn.ADJ
+        tag_map['V'] = wn.VERB
+        tag_map['R'] = wn.ADV
+
+        lmtzr = WordNetLemmatizer()
+        s = []
+        for token, tag in pos_tag(tokens):
+            lemma = lmtzr.lemmatize(token, tag_map[tag[0]])
+            s.append(lemma)
+            
+        return ' '.join(s)
+    
+    
+    def do_lemmatization(self):
+        self.my_dataframe['question1'] = self.my_dataframe['question1'].apply(self.lemmi_helper)
+        self.my_dataframe['question2'] = self.my_dataframe['question2'].apply(self.lemmi_helper)
+
+
+
+'''
+df = pd.read_csv("./train.csv")
+obj = text_processing(df)
+obj.tokenize()
+obj.remove_stop_words()
+obj.do_lemmatization()
+
+contents of df will be modified. 
+
+'''
